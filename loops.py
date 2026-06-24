1584a33c9c85584bf8f6278401f0aa16ce9e6742
Elyashiv <elyashiv988@gmail.com>
2026-06-24 18:12:55 +0300

    part1

---
+++ loops.py
@@ -0,0 +1,59 @@
+'''
+# part 1 
+# 1
+i= 0
+while i <=5:
+    print(i)
+    i+=1
+# 2
+i=10
+while i >=0:
+    print(i)
+    i=i-1
+# 3
+total=0
+while total<10:
+    total+=1
+print(total)
+# 4
+items = [2, 4, 6, 8]
+i=0
+while i < len(items)-1:
+    print(items[i])
+    i+=1
+    if items[i]>5:
+        break
+# 5
+
+i=0
+while i<=10:
+    if i%2==0:
+        print(i)
+    i=i+1
+# 6
+
+agents = ['Alpha', 'Bravo', 'Charlie']
+i=0
+while i<len(agents):
+    print(agents[i])
+    i+=1
+
+# 8
+i=1
+while i*2<100:
+   i=i*2
+print(i)
+# 9
+
+data = [3, 7, 2, -1, 5]
+i=0
+sum=0
+while i <len(data):
+    if data[i]==-1:
+        break
+   
+    sum=sum+data[i]
+    i=i+1
+print(sum)
+'''
+#10
